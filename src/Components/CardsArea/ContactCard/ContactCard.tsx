4827009fa9fd09df56ce22de68f5c4a2a4d5100f
Dor Abutbul <Doronabutbul@gmail.com>
2023-06-29 17:04:43 +0300

    added contact me

--- src/Components/CardsArea/ContactCard/ContactCard.tsx
+++ src/Components/CardsArea/ContactCard/ContactCard.tsx
@@ -1,5 +1,6 @@
 import { Row } from "react-bootstrap";
 import "./ContactCard.css";
+import { useEffect, useState } from "react";
 
 
 interface ContactCardProps {
@@ -7,16 +8,37 @@ interface ContactCardProps {
     image: string
     description: string
     url: string
+    bgColor: string
+    btnContent: string
 }
 
 function ContactCard(props: ContactCardProps): JSX.Element {
+
+    useEffect(() => {
+        if (props.name === "Phone Number") {
+
+            let CurrentHour = new Date().getHours()
+
+            if (CurrentHour <= 12 || CurrentHour >= 20) {
+                setDisabled("disabled")
+                setBgColor("grey")
+            }
+        }
+        
+    }, [])
+
+    const [disabled, setDisabled] = useState<string>("")
+    const [bgColor, setBgColor] = useState<string>(props.bgColor)
+
+
     return (
         <div className="ContactCard">
             <img src={props.image} />
             <Row>
-                <h3>{props.name}</h3>
+                <h5>{props.name}</h5>
                 <p>{props.description}</p>
             </Row>
+            <a href={props.url} className={disabled} target="_blank" style={{ backgroundColor: bgColor }}>{props.btnContent}</a>
         </div>
     );
 }
--- src/Components/HomeArea/ContactMe/ContactMe.tsx
+++ src/Components/HomeArea/ContactMe/ContactMe.tsx
@@ -1,11 +1,17 @@
 import ContactCard from "../../CardsArea/ContactCard/ContactCard";
 import "./ContactMe.css";
 import wsImage from "../../../Assets/Images/whatsapp.jpg"
+import fbImage from "../../../Assets/Images/Facebook Card.jpg"
+import liImage from "../../../Assets/Images/LinkedIn Card.jpg"
+import phoneNum from "../../../Assets/Images/Call Me Card.jpg"
 
 function ContactMe(): JSX.Element {
     return (
         <div className="ContactMe">
-            <ContactCard name="WhatsApp" image={wsImage} description="" url="" />
+            <ContactCard name="Facebook" image={fbImage} description="Feel free to reach me out on Facebook!" url="https://m.me/APerfectHunk" bgColor="#4b77cf" btnContent="Messanger Chat" />
+            <ContactCard name="WhatsApp" image={wsImage} description="Feel free to reach me out on Whatsapp!" url="https://wa.me/972527727614" bgColor="#009633" btnContent="WhatsApp Chat" />
+            <ContactCard name="LinkedIn" image={liImage} description="Feel free to reach me out on LinkedIn!" url="https://www.linkedin.com/in/dor-abutbul" bgColor="#4b77cf" btnContent="LinkdIn Chat" />
+            <ContactCard name="Phone Number" image={phoneNum} description="Call Me Between 12:00 and 20:00" url="tel:+972527727614" bgColor="#009633" btnContent="Call Me" />
         </div>
     );
 }
